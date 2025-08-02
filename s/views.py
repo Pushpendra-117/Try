87858691081bacd34df74ca5201aa60ac9bf476b
Pushpendra-117 <cdharmendrasiha@gmail.com>
2025-08-02 19:49:55 +0530

    Update views.py

--- s/views.py
+++ s/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request, "index.HTML", context)
+  return render(request, "index.HTML")
