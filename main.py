cad8747ba2d02b765937f3b47e4b032eff2791d6
Pushpendra-117 <cdharmendrasiha@gmail.com>
2025-08-02 15:47:50 +0530

    Update2 main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
+from T.wsgi import application as app
+
 # main.py
-
-from flask import Flask
-
-app = Flask(__app__)  # Yeh 'app' object Gunicorn ko chahiye hota hai
-
-@app.route("/")
-def home():
-    return "Hello from Flask in main.py!"
